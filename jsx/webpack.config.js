a382713c15fe20fbd3807adf2a56b7b74b2b7732
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2024-01-31 14:42:05 +0530

    JSX added with react,

---
+++ jsx/webpack.config.js
@@ -0,0 +1,13 @@
+// webpack.config.js
+module.exports = {
+  entry: "./index.jsx",
+  target: ["web", "es5"],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: "babel-loader",
+      },
+    ],
+  },
+};
