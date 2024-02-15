a4e22ceb0f22b5a4da912bf905a9cd89812bc2ab
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2024-02-15 18:10:36 +0530

    type asset behavior and modification,

--- webpack/dist/bundle.js
+++ webpack/dist/bundle.js
@@ -1,1 +1,131 @@
-(()=>{"use strict";var t={};t.g=function(){if("object"==typeof globalThis)return globalThis;try{return this||new Function("return this")()}catch(t){if("object"==typeof window)return window}}(),(()=>{var e;t.g.importScripts&&(e=t.g.location+"");var r=t.g.document;if(!e&&r&&(r.currentScript&&(e=r.currentScript.src),!e)){var c=r.getElementsByTagName("script");if(c.length)for(var i=c.length-1;i>-1&&!e;)e=c[i--].src}if(!e)throw new Error("Automatic publicPath is not supported in this browser");e=e.replace(/#.*$/,"").replace(/\?.*$/,"").replace(/\/[^\/]+$/,"/"),t.p=e})();const e=t.p+"32f4e60bfb708063a8fc.jpg";console.log({msg:"HW"}),function(){const t=document.createElement("img");t.alt="Kiwi",t.width=300,t.src=e,document.getElementById("body").appendChild(t)}()})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ hw)
+/* harmony export */ });
+// es6
+function hw() {
+  console.log({ msg: "HW" });
+}
+
+// Legacy/es5
+// function hw() {
+//   console.log({ msg: "HW" });
+// }
+// module.exports = hw;
+
+
+/***/ }),
+/* 2 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _kiwi_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(3);
+
+function addImg() {
+  const img = document.createElement("img");
+  img.alt = "Kiwi";
+  img.width = 300;
+  img.src = _kiwi_jpg__WEBPACK_IMPORTED_MODULE_0__;
+  const body = document.getElementById("body");
+  body.appendChild(img);
+}
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (addImg);
+
+
+/***/ }),
+/* 3 */
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "32f4e60bfb708063a8fc.jpg";
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		__webpack_require__.p = "http://192.168.1.100:5500/dist/";
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _hw__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+/* harmony import */ var _img__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2);
+// es6
+
+
+// Legacy/es5
+// const hw = require("./hw");
+(0,_hw__WEBPACK_IMPORTED_MODULE_0__["default"])();
+(0,_img__WEBPACK_IMPORTED_MODULE_1__["default"])();
+
+})();
+
+/******/ })()
+;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -5,13 +5,25 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "./dist"),
+    // publicPath: "dist/",
+    publicPath: "http://192.168.1.100:5500/dist/",
   },
-  mode: "production",
-  // mode: "none",
+  // mode: "production",
+  mode: "none",
   module: {
     // asset/inline, asset/resource, asset, asset/source
     rules: [
-      { test: /\.(png|jpg)$/, type: "asset/resource" },
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            // maxSize: 3 * 100 * 1024, //3kB
+          },
+        },
+      }, // By default this will treat size<8kB it will be inline otherwise it will be resource
+      // { test: /\.(png|jpg)$/, type: "asset/resource" },
+      // { test: /\.(png|jpg)$/, type: "asset/inline" },
       // { test: /\.(ttf)$/, type: "asset/resource" },
     ],
   },
