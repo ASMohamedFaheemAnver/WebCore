e6797fc407285c286e47b8426c5ef493f6d5a6da
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2024-03-04 18:13:54 +0530

    Hash to bundle and css file and clean dir plugin,

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,14 +1,21 @@
 // Until webpack 5, config doesn't support es5 features
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
+const path = require("path");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
+    filename: "bundle.[contenthash].js", // contenthash will generate new file name only if the content inside the file changes
     path: path.resolve(__dirname, "./dist"),
     // publicPath: "dist/",
     publicPath: "http://192.168.1.100:5500/dist/",
+    // clean: true, // Same as CleanWebpackPlugin with default config
+    // clean: {
+    //   dry: true,
+    //   keep: /\.css/,
+    // },
   },
   // mode: "production",
   mode: "none",
@@ -50,6 +57,12 @@ module.exports = {
   },
   plugins: [
     new TerserPlugin() /* TerserPlugin is installed by default in webpack5, Which use to minify the bundle, but I see mode=production also giving same results */,
-    new MiniCssExtractPlugin({ filename: "style.css" }),
+    new MiniCssExtractPlugin({ filename: "style.[contenthash].css" }), // contenthash will generate new file name only if the content inside the file changes
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        "**/*", // Default paths
+        path.join(process.cwd(), "build/**/*"), // Can add more paths like this
+      ], // This patterns related to output path we gave above in output.path
+    }), // To clean dist on restart
   ],
 };
