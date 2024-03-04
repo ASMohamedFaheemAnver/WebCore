8db2a948a10287a6c0e8accb6c2e89786fd6b66a
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2024-03-04 17:45:17 +0530

    Minify and css extract plugin,

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -3,7 +3,12 @@ import "./index.css";
 import "./override.scss";
 import hw from "./hw";
 import addImg from "./img";
+import Heading from "./components/Heading";
+
 // Legacy/es5
 // const hw = require("./hw");
 hw();
 addImg();
+
+const heading1 = new Heading();
+heading1.render();
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,7 @@
 // Until webpack 5, config doesn't support es5 features
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
@@ -26,10 +28,15 @@ module.exports = {
       // { test: /\.(png|jpg)$/, type: "asset/inline" },
       // { test: /\.(ttf)$/, type: "asset/resource" },
       { test: /\.(txt)$/, type: "asset/source" },
-      { test: /\.(css)$/, use: ["style-loader", "css-loader"] }, // css-loader reads the contents and return it, This will be inside bundle.js rather than separate file, style-loader will inject it to the dom
+      {
+        test: /\.(css)$/,
+        // use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"], // To output css separately
+      }, // css-loader reads the contents and return it, This will be inside bundle.js rather than separate file, style-loader will inject it to the dom
       {
         test: /\.(scss)$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        // use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.(js)$/,
@@ -41,4 +48,8 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new TerserPlugin() /* TerserPlugin is installed by default in webpack5, Which use to minify the bundle, but I see mode=production also giving same results */,
+    new MiniCssExtractPlugin({ filename: "style.css" }),
+  ],
 };
